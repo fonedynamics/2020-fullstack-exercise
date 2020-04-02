f6417b7b01de0865960943b3e6f10e6c756bddeb
Dominic Joseph <dominic.vattakunnel@fitlg.com>
2020-04-02 10:21:19 +1000

    Base project added

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace fonedynamics._2020.fullstack.exercise
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
