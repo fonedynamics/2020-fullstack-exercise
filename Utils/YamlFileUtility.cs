44bdf325c586a7e6832355c9ce6dfcfe9c534a1e
Dominic Joseph <dominic.vattakunnel@fitlg.com>
2020-04-02 10:43:24 +1000

    Utility function added to read from Yaml file

---
+++ Utils/YamlFileUtility.cs
@@ -0,0 +1,29 @@
+﻿
+#region Using
+
+using System.IO;
+using YamlDotNet.RepresentationModel;
+
+#endregion
+
+namespace fonedynamics._2020.fullstack.exercise.Utils
+{
+    public class YamlFileUtility
+    {
+        #region Public Methods
+
+        public static YamlSequenceNode ReadYamlFile(string filepath, string value)
+        {
+            var reader = new StreamReader(filepath);
+            var yamlStream = new YamlStream();
+            yamlStream.Load(reader);
+            var mapping =
+                (YamlMappingNode)yamlStream.Documents[0].RootNode;
+            var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode(value)];
+            return items;
+        }
+
+        #endregion
+
+    }
+}
