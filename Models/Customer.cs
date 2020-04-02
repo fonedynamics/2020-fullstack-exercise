b87d7e81f689d987d14cae853a667af8e8e67637
Dominic Joseph <dominic.vattakunnel@fitlg.com>
2020-04-02 10:32:04 +1000

    Backend Models added

---
+++ Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace _2020_fullstack_exercise.Models
+{
+    public class Customer
+    {
+        #region Properties
+
+        /// <summary>Gets or sets the customer id.</summary>
+        public string Id { get; set; }
+
+        /// <summary>Gets or sets the number of employees.</summary>
+        public int NumEmployees { get; set; }
+
+        /// <summary>Gets or sets the customer name.</summary>
+        public string Name { get; set; }
+
+        /// <summary>Gets or sets the tags.</summary>
+        public List<string> Tags { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿namespace _2020_fullstack_exercise.Models
+{
+    public class Login
+    {
+        #region Properties
+
+        /// <summary>Gets or sets the username.</summary>
+        public virtual string Username { get; set; }
+        /// <summary>Gets or sets the password.</summary>
+        public virtual string Password { get; set; }
+
+
+        #endregion
+    }
+}
