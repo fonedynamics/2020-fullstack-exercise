2259779479a248e4d58fd188db13496f6c78c66d
Dominic Joseph <dominic.vattakunnel@fitlg.com>
2020-04-02 11:28:30 +1000

    Component added to module and nav menu

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { CounterComponent } from './counter/counter.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -22,7 +24,8 @@ import { CounterComponent } from './counter/counter.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent }
+      { path: 'counter', component: CounterComponent },
+      { path: 'customer', component: CustomerComponent }
     ])
   ],
   providers: [],
